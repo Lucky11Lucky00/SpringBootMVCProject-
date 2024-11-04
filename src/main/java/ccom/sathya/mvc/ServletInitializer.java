722f33bb7571a9ffa2c7bb7919feccb407ecab2f
unknown <annapareddylakshmidurga34@gmail.com>
2024-11-04 10:13:07 +0530

    first commit

---
+++ src/main/java/ccom/sathya/mvc/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ccom.sathya.mvc;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootMvcProjectApplication.class);
+	}
+
+}
---
+++ src/test/java/ccom/sathya/mvc/SpringBootMvcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ccom.sathya.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
